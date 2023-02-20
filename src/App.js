b98197eb62aa7a174f704d8cc146ef13bbc93bdb
Daria Gerasimova <dashka_potter@icloud.com>
2023-02-20 20:03:37 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+import {data} from "./data";
+import { useState } from "react";
+import "./App.css";
+
+function App(){
+  const [books,setBooks]= useState(data);
+  const [showText,setShowText] = useState(false);
+
+  const removeBook = (id) => {
+    let newBook = books.filter((book) => book.id !== id);
+    setBooks(newBook)
+  }
+  const showTextClick = (item) =>{
+    item.showMore=!item.showMore 
+    setShowText(!showText)
+  }
+
+
+  return(
+  <div className="container">
+    <h1 className="titel">Top {books.length} must-read fantasy novels</h1>
+    {
+      books.map((item => {
+        const{id,name,image,description,showMore} = item;
+        return(
+          <div key={id} className="main">
+            <div className="container">
+              <h2>{id} - {name}</h2>
+            </div>
+            
+            <div className="container">
+              <img src={image} width="350px" alt="book"/>
+              </div>
+              <div className="container">
+              <p className="descrip">
+                {showMore ? description : description.substring(0,210)+"..."}
+                <button onClick={()=> showTextClick(item)}>{showMore ? "show less":"show more"}</button>
+              </p>
+            </div>
+            <div className="container-btn " >
+              <button className="btn" onClick={() => removeBook(id)}>Remove</button>
+            </div>
+            <div className="container">
+              
+            </div>
+          </div>
+        )
+      }))
+    }
+    <div className="container">
+      <button className="btn2" onClick={() => setBooks([])}>Remove all</button>
     </div>
-  );
+  </div>
+  )
 }
 
 export default App;
+
---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        id:1,
+        image:"https://images-na.ssl-images-amazon.com/images/I/816CWypmheL._AC_UL900_SR615,900_.jpg",
+        name:"Lockwood & Co. The Whispering Skull",
+        description:"In the six months since Anthony, Lucy, and George survived a night in the most haunted house in England, Lockwood & Co. hasn't made much progress. Quill Kipps and his team of Fittes agents keep swooping in on Lockwood's investigations. Finally, in a fit of anger, Anthony challenges his rival to a contest: the next time the two agencies compete on a job, the losing side will have to admit defeat in the Times newspaper.Things look up when a new client, Mr. Saunders, hires Lockwood & Co. to be present at the excavation of Edmund Bickerstaff, a Victorian doctor who reportedly tried to communicate with the dead. Saunders needs the coffin sealed with silver to prevent any supernatural trouble. All goes well-until George's curiosity attracts a horrible phantom.Back home at Portland Row, Lockwood accuses George of making too many careless mistakes. Lucy is distracted by urgent whispers coming from the skull in the ghost jar. Then the team is summoned to DEPRAC headquarters. Kipps is there too, much to Lockwood's annoyance. Bickerstaff's coffin was raided and a strange glass object buried with the corpse has vanished. Inspector Barnes believes the relic to be highly dangerous, and he wants it found.",
+        showMore:false
+    },
+    {
+        id:2,
+        image:'https://kbimages1-a.akamaihd.net/8c56227a-7c0b-4664-b54c-c9b94972954b/1200/1200/False/the-outlaws-scarlett-and-browne-1.jpg',
+        name:'The Outlaws Scarlett and Browne',
+        description:"Scarlett McCain is a shoot-first ask-questions-later kind of outlaw. She scrapes by on bank heists, her wits—and never looking back.She’s on the run from her latest crime when she comes across Albert Browne. He is the sole survivor of a horrific accident, and against her better judgement, Scarlett agrees to guide him to safety.This is a mistake. Soon there are men with dogs and guns and explosives hot on their heels. Scarlett’s used to being chased by the law, but this is extreme. It was only a little bank she’d robbed . . .As they flee together across the wilds, fighting off monstrous beasts, and dodging their pursuers, Scarlett comes to realize that Albert Browne is hiding a terrible secret. And that he may be the most dangerous threat of all.In this fast-paced, quick-witted whirlwind of a story, Jonathan Stroud introduces two unlikely allies—the outlaws Scarlett and Browne—who are about to become the most notorious renegades in all that’s left of Britain.",
+        showMore:false
+    },
+    {
+        id:3,
+        image:'https://m.media-amazon.com/images/I/51QZwNuO5kL.jpg',
+        name:"The Amulet of Samarkand",
+        description:"Nathaniel is a magician's apprentice, taking his first lessons in the arts of magic. But when a devious hot-shot wizard named Simon Lovelace ruthlessly humiliates Nathaniel in front of his elders, Nathaniel decides to kick up his education a few notches and show Lovelace who's boss. With revenge on his mind, he summons the powerful djinni, Bartimaeus. But summoning Bartimaeus and controlling him are two different things entirely, and when Nathaniel sends the djinni out to steal Lovelace's greatest treasure, the Amulet of Samarkand, he finds himself caught up in a whirlwind of magical espionage, murder, and rebellion.",
+        showMore:false
+    },
+    {
+        id:4,
+        image:'https://m.media-amazon.com/images/I/61K06DK3aVL._AC_UF1000,1000_QL80_.jpg',
+        name:"The Spellslinger",
+        description:"Kellen is moments away from facing his first duel and proving his worth as a spellcaster. There's just one problem: his magic is fading.Facing exile unless he can pass the mage trials, Kellen is willing to risk everything -- even his own life -- in search of a way to restore his magic. But when the enigmatic Ferius Parfax arrives in town, she challenges him to take a different path.One of the elusive Argosi, Ferius is a traveller who lives by her wits and the cards she carries. Daring, unpredictable, and wielding magic Kellen has never seen before, she may be his only hope.The first novel in a compelling six-book series, bursting with tricks, humor, and a whole new way to look at magic.",
+        showMore:false
+    },
+    {
+        id:5,
+        image:'https://www.logobook.ru/make_nimage.php?uid=14802756',
+        name:"Cinder",
+        description:"Humans and androids crowd the raucous streets of New Beijing. A deadly plague ravages the population. From space, a ruthless lunar people watch, waiting to make their move. No one knows that Earth’s fate hinges on one girl....Cinder, a gifted mechanic, is a cyborg. She’s a second-class citizen with a mysterious past, reviled by her stepmother and blamed for her stepsister’s illness. But when her life becomes intertwined with the handsome Prince Kai’s, she suddenly finds herself at the center of an intergalactic struggle, and a forbidden attraction. Caught between duty and freedom, loyalty and betrayal, she must uncover secrets about her past in order to protect her world’s future.",
+        showMore:false
+    }
+   
+]
